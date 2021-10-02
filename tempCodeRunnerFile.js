5a556e6703d7bbd1718751c7be578138ed48958c
rishabh2001-py <rishabhtripathi.tripathi345@gmail.com>
2021-10-02 08:20:19 +0530

    new

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+const wordInput = document.querySelector('#word-input');
+// const currentWord = document.querySelector('#current-word');
+// const scoreDisplay = document.querySelector('#score');
+// const timeDisplay = document.querySelector('time');
+// const message = document.querySelector('#message');
+// const seconds = document.querySelector('#seconds');  
