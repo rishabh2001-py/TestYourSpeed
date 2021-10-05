ce87556cd1804cb470064ba72d6cdea8d9c5501f
rishabh2001-py <rishabhtripathi.tripathi345@gmail.com>
2021-10-05 07:37:54 +0530

    updated js

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-window.addEventListener('load',init)
+window.addEventListener('load',init) ;
 
 // Globals
 
-let time = 5 ;
+// levels 
+const levels = {
+  easy : 5,
+  medium : 3,
+  hard : 1
+}
+
+const currentLevel =  levels.medium;
+
+let time = currentLevel ;
 let score = 0;
 let isPlaying;
 
@@ -11,7 +20,7 @@ let isPlaying;
 const wordInput = document.querySelector('#word-input');
 const currentWord = document.querySelector('#current-word');
 const scoreDisplay = document.querySelector('#score');
-const timeDisplay = document.querySelector('time');
+const timeDisplay = document.querySelector('#time');
 const message = document.querySelector('#message');
 const seconds = document.querySelector('#seconds');  
 
@@ -37,16 +46,84 @@ var myArray = [
   ];
 
 function init(){
+
+    seconds.innerHTML = currentLevel;
     showWords(myArray);
+    // start matching 
+    wordInput.addEventListener('input',startMatch);
+
+    // call countDown
+
+    setInterval(countDown,1000);
+    setInterval(checkStatus,50);
     
 }
+
+function startMatch(){
+
+  if (matchWords()){
+    
+    isPlaying = true;
+    time  = currentLevel + 1; 
+    showWords(myArray);
+    wordInput.value = '';
+    score++;
+  }
+  else if(score > 2){
+    currentLevel = currentLevel.hard;
+  }
+  
+  if (score == -1){
+    scoreDisplay.innerHTML = 0 ;
+  }
+  else{
+    scoreDisplay.innerHTML = score; 
+  }
+  
+    
+}
+
+function matchWords(){
+  if(wordInput.value == currentWord.innerHTML){
+    message.innerHTML = "Correct !!";
+    return true;
+  }
+  else{
+     message.innerHTML = '';
+     return false;
+     
+  }  
+
+}
+
+
 
 function showWords(myArray){
     const Randword  = myArray[Math.floor(Math.random()*myArray.length)];
     currentWord.innerHTML = Randword;
-
 }
 
 
 
+function countDown(){
+
+    if(time > 0){
+      time--;
+    }
+    else if(time==0){
+      isPlaying = false;
+    }
+
+    timeDisplay.innerHTML = time;
+    console.log(time);
+}
+
+
+function checkStatus(){
+  if (!isPlaying && time == 0){
+    message.innerHTML = "Game Over !!!";
+    score = -1;
+  }
+
+}
 
